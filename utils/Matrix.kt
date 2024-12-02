d1a373ee39558b41ea56eacf2289cba5637c5b63
Ilia Scholl <ilia.scholl@up42.com>
2024-12-02 12:21:55 +0100

    Day 02

--- utils/Matrix.kt
+++ utils/Matrix.kt
@@ -5,6 +5,8 @@ typealias Matrix<T> = List<List<T>>
 
 fun readStringMatrix(name: String): Matrix<String> = Path("$name.txt").readLines().map { it.split(Regex("\\s+")) }
 
+fun readIntMatrix(name: String): Matrix<Int> = readStringMatrix(name).map { it.map(String::toInt) }
+
 fun <T> Matrix<T>.transpose(): Matrix<T> =
     first().indices.map { colIndex ->
         indices.map { rowIndex -> this[rowIndex][colIndex] }
