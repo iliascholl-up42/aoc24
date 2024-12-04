601442333364e55b61bd5d74430e354d67ee1f81
Ilia Scholl <ilia.scholl@up42.com>
2024-12-04 12:04:59 +0100

    Day 04

--- src/input/Input.kt
+++ src/input/Input.kt
@@ -1,6 +1,28 @@
 package input
 
+import matrix.Matrix
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.reflect.KProperty
+import kotlin.streams.toList
 
 fun readString(vararg names: String) = names.map { Path("$it.txt").readText() }
+
+class FileDelegate<T>(
+    private val transformer: (String) -> T,
+) {
+    operator fun getValue(
+        thisRef: Any?,
+        property: KProperty<*>,
+    ) = Path("${property.name}.txt").readText().trim().let(transformer)
+}
+
+val file = FileDelegate { it }
+
+fun <T> file(transformer: (String) -> T) = FileDelegate(transformer)
+
+object Transformers {
+    val charMatrix: (String) -> Matrix<Char> = { input ->
+        input.lines().map { it.chars().toList().map(Int::toChar) }
+    }
+}
